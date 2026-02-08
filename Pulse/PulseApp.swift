265293102143a2e88248d726ed2ebe1b49d5d33a
pratikbhavarthe <pratikbhavarthee1998@gmail.com>
2026-02-08 23:34:37 +0530

    Initial Commit

---
+++ Pulse/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Pulse
+//
+//  Created by Pratik Bhavarthe on 08/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
